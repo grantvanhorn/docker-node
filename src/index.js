0f6d5ecc61b3d9dc5dfa8a62c76c62db5be9227b
Grant Van Horn <grant.vanhorn@tinroofsoftware.com>
2019-05-27 15:07:24 -0400

    * Add override config. Move settings from base config to override

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello this is more\n')
+  res.send('Hello this is more: ' + process.env.DEBUG + '\n');
 })
 
-app.listen(3000)
+app.listen(3000);
